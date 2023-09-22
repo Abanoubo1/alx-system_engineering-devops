b1fe49ee7e3ad44e258ca5702f3d93a3640b5970
Abanoubo1 <abanoubwageh88@gmail.com>
2023-09-22 11:40:22 -0700

    some fix

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,7 @@
-#Create file named school in /tmp directory
-file { '/tmp/school':
-  ensure 	=> file,
-  content 	=> 'I love Puppet',
-  mode 		=> '0744',
-  owner		=> 'www-data',
-  group 	=> 'www-data',
-}
+# Creating A file in tmp folder named holberton
+file { '/tmp/holberton':
+    ensure  => 'present',
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
+    content => 'I love Puppet', }
