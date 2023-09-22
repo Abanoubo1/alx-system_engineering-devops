c9560a15d6685eeb4c676ff19fe61001f62bb7df
Abanoubo1 <abanoubwageh88@gmail.com>
2023-09-22 11:54:40 -0700

    help

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,8 @@
 # Creating A file in tmp
 file { '/tmp/school':
-    ensure  => 'present',
-    mode    => '0744',
-    owner   => 'www-data',
-    group   => 'www-data',
-    content => 'I love Puppet', }
+	ensure  => 'present',
+	path    => '/tmp/school',
+	mode    => '0744',
+	owner   => 'www-data',
+	group   => 'www-data',
+	content => 'I love Puppet', }
